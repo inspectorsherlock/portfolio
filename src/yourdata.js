2c72d7e4e999a993313738a1b765b8474536111b
Fawwaz <fawwaz.chowdhury@googlemail.com>
2021-12-24 17:53:07 +0000

    Add files via upload

---
+++ src/yourdata.js
@@ -0,0 +1,215 @@
+// Skills Icons
+import htmlIcon from "./images/html.svg"
+import cssIcon from "./images/css.svg"
+import reactIcon from "./images/react.svg"
+import jsIcon from "./images/javascript.svg"
+import pythonIcon from "./images/python.svg"
+import sqlIcon from "./images/sql.svg"
+import flaskIcon from "./images/flask.svg"
+import pixiIcon from "./images/pixijs.svg"
+import illustratorIcon from "./images/illustrator.png"
+
+// Social Icon
+import githubIcon from "./images/github.svg"
+import linkedinIcon from "./images/linkedin.svg"
+import twitterIcon from "./images/twitter.svg"
+import instagramIcon from "./images/instagram.svg"
+
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "chetan",
+  headerTagline: [
+    //Line 1 For Header
+    "Building digital",
+    //Line 2 For Header
+    "products, brands,",
+    //Line 3 For Header
+    "and experience",
+  ],
+  //   Header Paragraph
+  headerParagraph:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
+
+  //Contact Email
+  contactEmail: "fawwaz.chowdhury@googlemail.com",
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      title: "Dissertation Project", //Project Title - Add Your Project Title Here
+      para:
+        "Data Acquisition of Novel Learning Theory Ontology and Implementation of Education Recommender", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1481627834876-b7833e8f5570?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZWR1Y2F0aW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/inspectorsherlock/studybuddyapp-deploy",
+    },
+    {
+      title: "Database Project", //Project Title - Add Your Project Title Here
+      para:
+        "SQL Database of a Library", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1597852074816-d933c7d2b988?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/inspectorsherlock/SQL-Library-Database-Project",
+    },
+    {
+      title: "Data Analysis Project", //Project Title - Add Your Project Title Here
+      para:
+        "Data Analysis of Education Results with application of two Machine Learning Algorithms", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1543286386-713bdd548da4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YW5hbHlzaXN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/inspectorsherlock/Data-Analysis",
+    },
+    {
+      title: "Covid Data Analysis Project", //Project Title - Add Your Project Title Here
+      para:
+        "Covid Tracker Application", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1584036561566-baf8f5f1b144?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y292aWR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/inspectorsherlock/Covid-Tracker-Dashboard",
+    },
+    {
+      title: "SmartMind", //Project Title - Add Your Project Title Here
+      para:
+        "Updated Student Companion Application Based on Dissertation Project", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://media.istockphoto.com/photos/project-management-concept-with-gantt-chart-picture-id1279502184?b=1&k=20&m=1279502184&s=170667a&w=0&h=sYKVYiIzoHr6DxHoKRQEFtOUr_KeE_0upSqa0VsWwLE=",
+      //Project URL - Add Your Project Url Here 
+      url: "https://github.com/inspectorsherlock",
+    },
+    {
+      title: "Paradigm", //Project Title - Add Your Project Title Here Student Companion Application
+      para:
+        "Currently Developing an Adaptive Courseware Program", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://media.istockphoto.com/photos/elearning-education-concept-learning-online-picture-id1290864946?b=1&k=20&m=1290864946&s=170667a&w=0&h=zZq7rG5McSptSIpEm9f8iTGd3Mrdkcslakr91T7qTYM=",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/inspectorsherlock",
+    },
+
+    /*
+    If You Want To Add More Project just Copy and Paste This At The End
+    ,{
+        title: 'Project Five',
+        para: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // About Secton --------------
+  aboutParaOne:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+  aboutParaTwo:
+    "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  aboutParaThree:
+    "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  aboutImage:
+    "https://media-exp1.licdn.com/dms/image/C4D03AQFKHhdpcub0JQ/profile-displayphoto-shrink_800_800/0/1548942446417?e=1645660800&v=beta&t=JEiwbjFYJaiypwJs8LkQ9FO_YNs_MvMccfcIkO7hwxA",
+
+  //   End About Section ---------------------
+
+  // Skills Section ---------------
+
+  //   Import Icons from the top and link it here
+
+  skills: [
+    {
+      img: htmlIcon,
+      para:
+        "Developed with HTML5",
+    },
+    {
+      img: cssIcon,
+      para:
+        "Developed with CSS3",
+    },
+    {
+      img: jsIcon,
+      para:
+      "Developed with JavaScript",
+    },
+    {
+      img: reactIcon,
+      para:
+        "Developed with ReactJS",
+    },
+    {
+      img: pythonIcon,
+      para:
+        "Developed with Python",
+    },
+    {
+      img: illustratorIcon,
+      para:
+        "Created with Adobe Illustrator",
+    },
+    {
+      img: sqlIcon,
+      para:
+        "Developed Databases with SQL",
+    },
+    {
+      img: flaskIcon,
+      para:
+        "Developed with Flask",
+    },
+    {
+      img: pixiIcon,
+      para:
+        "Created with Pixi.JS"
+    },
+  ],
+
+  // End Skills Section --------------------------
+
+  //   Promotion Section --------------------------
+
+  promotionHeading: "Hobbies",
+  promotionPara:
+    "Spare time I love playing video games, watching films and keeping up-to-date with the latest advances in tech. I also write in a blog about my latest projects and side projects. These side projects concern analysing source codes for video games, mainly on 2D games, in conjunction with machine learning algorithms.",
+  // End Promotion Section -----------------
+
+  //   Contact Section --------------
+
+  contactSubHeading: "I'm excited to work on your projects",
+  social: [
+    // Add Or Remove The Link Accordingly
+    { img: githubIcon, 
+      url: "https://github.com/InspectorSherlock" 
+    },
+    {
+      img: linkedinIcon,
+      url: "https://www.linkedin.com/in/fawwazchowdhury",
+    },
+    {
+      img: twitterIcon,
+      url: "https://twitter.com/fawwazchowdhury",
+    },
+    {
+      img: instagramIcon,
+      url: "https://www.instagram.com/fawwazchowdhury",
+    },
+  ],
+
+  // End Contact Section ---------------
+}
+
+// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
